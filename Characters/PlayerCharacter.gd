f5df7d2de5f1779053e72b754945e1f7746562ac
TeeNik <teenik3315@gmail.com>
2023-05-02 04:21:08 +0600

    character movement

---
+++ Characters/PlayerCharacter.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var MovementSpeed : float = 100;
+
+func _physics_process(_delta):
+	var InputDir = Vector2(
+		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
+		Input.get_action_strength("Down") - Input.get_action_strength("Up"),
+	)
+	
+	velocity = InputDir * MovementSpeed
+	
+	move_and_slide()
+	
