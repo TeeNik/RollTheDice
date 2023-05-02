c5b1bc7d7c2d51d72220225490c33848c0a8d1d2
TeeNik <teenik3315@gmail.com>
2023-05-03 04:49:47 +0600

    character animation

--- Characters/PlayerCharacter.gd
+++ Characters/PlayerCharacter.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var MovementSpeed : float = 100;
 
+@onready var AnimationTree = $AnimationTree
+
 func _physics_process(_delta):
 	var InputDir = Vector2(
 		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
