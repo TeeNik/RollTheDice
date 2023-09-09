6ef25b62d9e031ac8cf82ede3b46cef8e5bcd067
TeeNik <teenik3315@gmail.com>
2023-09-09 11:51:41 +0600

    test json write

--- src/PresetManager.cpp
+++ src/PresetManager.cpp
@@ -6,6 +6,15 @@ void PresetManager::savePreset(const std::string& presetName)
 	const string filePath = PRESET_DIR + presetName + ".json";
 	ofFile file(filePath);
 
+	Json::Value ev;
+	ev["Account"]["name"] = "John123";
+	ev["Account"]["surname"] = "Smith";
+
+	std::cout << ev << std::endl;
+
+	ofxJSONElement json(ev);
+	bool result = json.save("test.json");
+
 	if (!file.exists()) {
 		std::cout << "The file " + filePath + " is missing\n";
 		file.create();
@@ -14,6 +23,8 @@ void PresetManager::savePreset(const std::string& presetName)
 	{
 		std::cout << "The file " + filePath + " is found\n";
 	}
+
+	//file.write(ev.asString().c_str(), ev.asString().size());
 }
 
 void PresetManager::loadPreset(const std::string& presetName)
--- src/PresetManager.h
+++ src/PresetManager.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 
+#include "ofxJSONElement.h"
+
 using std::string;
 
 #define PRESET_DIR "presets/"
@@ -10,6 +12,8 @@ class PresetManager
 public:
 	static void savePreset(const string& presetName);
 	static void loadPreset(const string& presetName);
+
+	ofxJSONElement jsonElement;
 
 };
 
